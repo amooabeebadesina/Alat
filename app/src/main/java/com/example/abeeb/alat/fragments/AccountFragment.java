6318cb798111e9176f7b73cac80c94a63e76acc6
Amoo Abeeb <amooabeeb0@gmail.com>
2017-08-30 02:45:24 +0100

    structuring

---
+++ app/src/main/java/com/example/abeeb/alat/fragments/AccountFragment.java
@@ -0,0 +1,10 @@
+package com.example.abeeb.alat.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by ABEEB on 8/30/2017.
+ */
+
+public class AccountFragment extends Fragment {
+}
---
+++ app/src/main/java/com/example/abeeb/alat/models/Account.java
@@ -0,0 +1,8 @@
+package com.example.abeeb.alat.models;
+
+/**
+ * Created by ABEEB on 8/30/2017.
+ */
+
+public class Account {
+}

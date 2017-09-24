db15b0ddef7fcf9e2f2cc92462b0b5c90ee83eac
= <=>
2017-09-24 04:03:27 +0100

    full login page

---
+++ app/src/main/java/com/example/abeeb/alat/activities/Regiister2Activity.java
@@ -0,0 +1,15 @@
+package com.example.abeeb.alat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.abeeb.alat.R;
+
+public class Regiister2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_regiister2);
+    }
+}

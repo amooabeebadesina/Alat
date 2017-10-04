093ca8008b09c783b65e4d81c8b0a82acc474125
= <=>
2017-10-04 07:49:46 +0100

    Half Slider

---
+++ app/src/main/java/com/example/abeeb/alat/activities/Onboarding1.java
@@ -0,0 +1,13 @@
+package com.example.abeeb.alat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Onboarding1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding1);
+    }
+}
